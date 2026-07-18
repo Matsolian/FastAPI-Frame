8c5f3f454df92d7133755966ebe87f6d7768731e
maaric-linux <holyloran@gmail.com>
2026-07-18 14:14:04 +0300

    init __init__ for dependencies

---
+++ FastAPI-Application/api/dependencies/__init__.py
@@ -0,0 +1,9 @@
+all = {
+    "get_users_db",
+    "get_database_strategy",
+    "get_access_tokens_db",
+}
+
+from .users import get_users_db
+from .strategy import get_database_strategy
+from .access_tokens import get_access_tokens_db
