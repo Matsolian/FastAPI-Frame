376a5dd32f70632c5fe0a2a981fbe56a3ad7f35a
maaric-linux <holyloran@gmail.com>
2026-07-18 15:13:22 +0300

    create user schemas

---
+++ FastAPI-Application/core/schemas/user.py
@@ -0,0 +1,14 @@
+from fastapi_users import schemas
+from core.models.types.user_id import UserIdType
+
+
+class UserRead(schemas.BaseUser[UserIdType]):
+    pass
+
+
+class UserCreate(schemas.BaseUserCreate):
+    pass
+
+
+class UserUpdate(schemas.BaseUserUpdate):
+    pass
